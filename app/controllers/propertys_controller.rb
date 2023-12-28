0cd62a6796b355a460069c8c251a8496e5f7b5d0
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-12-28 16:36:42 -0600

    Add controllers for property and img

---
+++ app/controllers/propertys_controller.rb
@@ -0,0 +1,48 @@
+class PropertysController < ApplicationController
+  before_action :set_property, only: %i[show update destroy]
+  
+  def index
+    @properties = Property.all
+  end
+
+  def show
+  end
+
+  def new
+    @property = Property.new
+  end
+
+  def create
+    @property = Property.new(property_params)
+    if @property.save
+      redirect_to @property, notice: success_message('created')
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @property.update(property_params)
+      redirect_to @property, notice: success_message('updated')
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @property.destroy
+    redirect_to properties_url, notice: success_message('deleted')
+  end
+
+  private
+
+  def property_params
+    params.require(:property).permit(
+      :title, :rent, :price, :dollars, :commune_id, :address, :area, :rooms, :bathrooms, :description, images: []
+    )
+  end
+
+  def set_property
+    @property = Property.find(params[:id])
+  end
+end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,16 @@
 class Property < ApplicationRecord
-  belongs_to :commune
   belongs_to :user
+  has_many_attached :images
+  belongs_to :commune
+
+  validates :title, presence: true
+  validates :rent, inclusion: { in: [true, false] }
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :dollars, inclusion: { in: [true, false] }
+  validates :commune, presence: true
+  validates :address, presence: true
+  validates :area, presence: true, numericality: { greater_than: 0 }
+  validates :rooms, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :bathrooms, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :description, presence: true
 end
