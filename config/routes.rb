c06c207e6542de74ca0d9c383ae54ea52de0a03f
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-12-28 17:00:48 -0600

    Add controller and route for communes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root 'propertys#index'
+
   devise_for :users
   resources :propertys
   resources :imgs, only: [:index, :show, :new, :create]
+  resources :commune, only: [:show]
 end
