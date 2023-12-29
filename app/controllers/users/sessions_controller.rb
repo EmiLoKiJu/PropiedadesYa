20c80d0722eb3e3fa4cdb3b39cd7f91ee160dc7d
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-12-28 21:11:26 -0600

    Add user show route and controller

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  before_action :authenticate_user!, only: [:show]
+
+  def show
+    @user = current_user
+  end
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  def after_sign_in_path_for(resource)
+    user_profile_path(current_user)
+  end
+
+  protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   root 'properties#index'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    get 'users/show', to: 'users/sessions#show', as: :user_profile
+    get "/custom_sign_out" => "devise/sessions#destroy", as: :custom_destroy_user_session
+  end
   resources :properties
   resources :commune, only: [:show]
 end
