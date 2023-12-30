4bceedbef6978df6276e6dc40d86542f04215262
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-12-29 22:35:55 -0600

    Add dolar value making a call with HTTParty

--- Gemfile
+++ Gemfile
@@ -50,6 +50,8 @@ gem 'pagy', "~> 5.10"
 
 gem 'cancancan'
 
+gem 'httparty'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,6 +6,14 @@ class Users::SessionsController < Devise::SessionsController
   def show
     @user = current_user
     @user_properties = @user.properties
+
+    api_data = ApiService.fetch_dolar_value
+
+    if api_data
+      @dolar = api_data
+    else
+      flash[:error] = 'Failed to retrieve data from the API'
+    end
   end
 
   # GET /resource/sign_in
